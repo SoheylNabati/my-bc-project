cfe91302b50c6f69b29b3d1eb997d5e0a35674c4
soheyl <soheyl.nabati3@gmail.com>
2022-10-10 14:40:11 +0100

    refactored my code

--- controllers/newsControllers.js
+++ controllers/newsControllers.js
@@ -1,7 +1,6 @@
 const { fetchTopics } = require("../models/newsModels");
 exports.getTopics = (req, res, next) => {
   return fetchTopics().then((topics) => {
-    console.log(topics);
     return res.status(200).send({ topics });
   });
 };

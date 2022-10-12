0f30122010868023a0e782c713d952b0031a5b56
soheyl <soheyl.nabati3@gmail.com>
2022-10-12 14:07:24 +0100

    redafctored models and added functionality to get comments count from comments table

--- models/newsModels.js
+++ models/newsModels.js
@@ -8,7 +8,14 @@ exports.fetchTopics = () => {
 
 exports.fetchArticle = (id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id=$1;`, [id])
+    .query(
+      `SELECT articles.*, Count(comments.comment_id) ::INT AS comment_count FROM articles
+    LEFT JOIN comments
+    ON comments.article_id=articles.article_id
+    WHERE articles.article_id=$1
+    GROUP BY articles.article_id;`,
+      [id]
+    )
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ msg: "Article Not Found", status: 404 });
